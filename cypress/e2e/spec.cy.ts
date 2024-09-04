5c21644a6881c6a88d5c9a9f4489c87a96aa6919
hakyoung12 <gkr097@naver.com>
2024-09-04 09:02:33 +0900

    test: cypress 테스트 코드 추가

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,15 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://example.cypress.io');
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('“Get started by” 라는 텍스트가 존재하는 것 검증', () => {
+    cy.contains('Get started by editing');
+  });
+
+  it('“Docs”라는 anchor heading이 있고 이 anchor의 href 속성이 정확하게 설정돼 있는지 검증', () => {
+    cy.contains('Docs')
+      .should('have.attr', 'href')
+      .and('include', 'https://nextjs.org/docs');
   });
 });
